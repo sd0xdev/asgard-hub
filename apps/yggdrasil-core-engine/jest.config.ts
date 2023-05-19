70e999645c081e438a8e4edeccb9ca4471bf7c44
sd0 <software.develop.0x@gmail.com>
2023-05-19 10:53:35 +0800

    Initial commit

---
+++ apps/yggdrasil-core-engine/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'yggdrasil-core-engine',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/yggdrasil-core-engine',
+};
