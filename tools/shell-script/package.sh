fcda73ac9f1b8098cffb8387b1ee412721a003a3
sd0 <software.develop.0x@gmail.com>
2023-08-27 13:51:23 +0800

    fix: script
    f
    
    f

--- tools/shell-script/package.sh
+++ tools/shell-script/package.sh
@@ -7,7 +7,7 @@ if [ "${CI}" = true ] ; then
     /kaniko/executor \
             --context "${CI_PROJECT_DIR}" \
             --dockerfile "${CI_PROJECT_DIR}/apps/${PROJECT_NAME}/Dockerfile" \
-            --destination "${CI_REGISTRY_IMAGE}/yu-gpt-engine-image:${CI_COMMIT_SHA}" \
+            --destination "${CI_REGISTRY_IMAGE}/${PROJECT_NAME}:${CI_COMMIT_SHA}" \
             --cache=false \
             --cleanup \
             --build-arg "SERVER_VERSION=${CI_COMMIT_SHORT_SHA}"
